fdee5c90564b959f13e127c01f1a33ca0d4b54e5
nvanonim <nvanon9@gmail.com>
2022-10-14 19:32:14 +0700

    feat: add route controller for book

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,24 +1,93 @@
 package controllers
 
 import (
-	"encoding/json"
-	"fmt"
+	"net/http"
+	"strconv"
+
 	"github.com/gorilla/mux"
 	"github.com/nvanonim/go-bookstore/pkg/models"
 	"github.com/nvanonim/go-bookstore/pkg/utils"
-	"net/http"
-	"strconv"
 )
-
-var NewBook models.Book
 
 func CreateBook(w http.ResponseWriter, r *http.Request) {
 	var book models.Book
-	err := json.NewDecoder(r.Body).Decode(&book)
+	err := utils.ParseBody(r, &book)
 	if err != nil {
 		utils.RespondWithError(w, http.StatusBadRequest, "Invalid request payload")
 		return
 	}
 	book.CreateBook()
 	utils.RespondWithJson(w, http.StatusCreated, book)
+}
+
+func GetAllBooks(w http.ResponseWriter, r *http.Request) {
+	books := models.GetAllBooks()
+	if books == nil {
+		utils.RespondWithError(w, http.StatusNotFound, "No books found")
+		return
+	}
+	utils.RespondWithJson(w, http.StatusOK, books)
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId, err := strconv.ParseInt(vars["bookId"], 0, 0)
+	if err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, "Invalid book ID")
+		return
+	}
+	book := models.GetBookById(bookId)
+	if book == nil {
+		utils.RespondWithError(w, http.StatusNotFound, "Book not found")
+		return
+	}
+	utils.RespondWithJson(w, http.StatusOK, book)
+}
+
+func UpdateBookById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	// Cek apakah IDnya ada di parameter
+	bookId, err := strconv.ParseInt(vars["bookId"], 0, 0)
+	if err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, "Invalid book ID")
+		return
+	}
+
+	// Cek apakah buku dengan ID tsb ada
+	book := models.GetBookById(bookId)
+	if book == nil {
+		utils.RespondWithError(w, http.StatusNotFound, "Book not found")
+		return
+	}
+
+	// Cek apakah request body valid/cocok dengan structnya
+	err = utils.ParseBody(r, &book)
+	if err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+
+	// Update buku
+	book.UpdateBook()
+	utils.RespondWithJson(w, http.StatusOK, book)
+}
+
+func DeleteBookById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	bookId, err := strconv.ParseInt(vars["bookId"], 0, 0)
+	if err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, "Invalid book ID")
+		return
+	}
+
+	book := models.GetBookById(bookId)
+	if book == nil {
+		utils.RespondWithError(w, http.StatusNotFound, "Book not found")
+		return
+	}
+
+	models.DeleteBookById(bookId)
+	utils.RespondWithJson(w, http.StatusOK, book)
 }
